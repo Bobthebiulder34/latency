9d98387d897822d733cdac17446dd774cd61b034
willrob <96406582+Bobthebiulder34@users.noreply.github.com>
2025-09-25 21:38:35 -0500

    Update up.js

--- up.js
+++ up.js
@@ -1,10 +1,15 @@
 // Function that runs when this script is loaded
 function showParticleAlert(message) {
-alert("Particle says: " + message);
+    alert("Particle says: " + message);
 }
 
 // Data received from Particle (from IFTTT Value1)
 const particleData = {
-message: "{{Value1}}",
-timestamp: "{{OccurredAt}}"
+    message: "{{Value1}}",
+    timestamp: "{{OccurredAt}}"
 };
+
+// Function that button calls
+function up() {
+    showParticleAlert(particleData.message);
+}
